59667db230ec4de795847e4f539ed0227a3d2429
maartendek <dekker.maarten@gmail.com>
2021-11-02 15:45:27 +0100

    simplification

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const isProduction = process.env.NODE_ENV == "production";
@@ -67,7 +66,6 @@ const config = {
 module.exports = () => {
     if (isProduction) {
         config.mode = 'production';
-        config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
     } else {
         config.mode = 'development';
     }
